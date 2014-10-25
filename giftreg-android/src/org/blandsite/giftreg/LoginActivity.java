73a3cb6a7562b72db6ec251e1c76d670c96eb506
Brandon Knitter <knitterb@blandsite.org>
2014-10-25 11:26:36 -0700

    adding a new project that works this time

---
+++ giftreg-android/src/org/blandsite/giftreg/LoginActivity.java
@@ -0,0 +1,42 @@
+package org.blandsite.giftreg;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		RelativeLayout rLayout = (RelativeLayout) findViewById (R.id.relativeLayoutWelcomeScr);
+		Resources res = getResources(); //resource handle
+		Drawable drawable = res.getDrawable(R.drawable.gifts1); //new Image that was added to the res folder
+		rLayout.setBackground(drawable);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//getMenuInflater().inflate(R.menu.login, menu);
+		return false;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
